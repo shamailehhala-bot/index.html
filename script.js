59100ed786d33e407bb398d8dcfe6442d1459196
Halashamaileh-Fullstack <shamailehhala@gmail.com>
2026-01-05 19:30:00 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function bookTicket() {
+    const tripName = "Avengers";
+    const time = "8:00 مساءً";
+
+    const message = `🎬
+نتمنالك رحلة ممتعة , تم حجز تذكرتك بنجاح!
+الرحلة: ${tripName}
+الوقت: ${time}`;
+
+    alert(message);
+}
